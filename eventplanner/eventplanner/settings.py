f95bc8bacaad8f74d803c7a492337900403ea9c2
Владас Куодис <vlad250577@yandex.ru>
2025-05-02 14:15:29 +0300

    Fix import isort

--- eventplanner/eventplanner/settings.py
+++ eventplanner/eventplanner/settings.py
@@ -1,4 +1,3 @@
-import os
 """
 Django settings for eventplanner project.
 
@@ -11,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
