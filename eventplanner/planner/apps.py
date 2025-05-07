6830f2b8061a26f61509d1cd7b185b0756d32156
Владас Куодис <vlad250577@yandex.ru>
2025-05-07 20:18:05 +0300

    Apdate admin panel

--- eventplanner/planner/apps.py
+++ eventplanner/planner/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PlannerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'planner'
+    verbose_name = 'Каталог мероприятий'
--- eventplanner/planner/models.py
+++ eventplanner/planner/models.py
@@ -2,23 +2,39 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    date_time = models.DateTimeField()
-    location = models.CharField(max_length=200)
-    company_info = models.TextField()
-    dress_code = models.CharField(max_length=200)
+    title = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    date_time = models.DateTimeField(verbose_name="Дата проведения")
+    location = models.CharField(max_length=200, verbose_name="Место проведения")
+    company_info = models.TextField(verbose_name="Информация о компании организаторе")
+    dress_code = models.CharField(
+        max_length=200, verbose_name="Информация о компании организаторе"
+    )
+
+    class Meta:
+        verbose_name = "объект «Мероприятие»"
+        verbose_name_plural = "Мероприятия"
 
     def __str__(self):
         return self.title
 
 
 class TelegramUser(models.Model):
-    user_id = models.CharField(max_length=50, unique=True)
-    username = models.CharField(max_length=100, blank=True, null=True)
-    first_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
-    subscribed = models.BooleanField(default=True)
+    user_id = models.CharField(max_length=50, unique=True, verbose_name="id")
+    username = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Юзернейм"
+    )
+    first_name = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Имя"
+    )
+    last_name = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Фамилия"
+    )
+    subscribed = models.BooleanField(default=True, verbose_name="Активен")
+
+    class Meta:
+        verbose_name = 'объект "Пользователь Telegram"'
+        verbose_name_plural = "Пользователи Telegram"
 
     def __str__(self):
         return self.username if self.username else str(self.user_id)
